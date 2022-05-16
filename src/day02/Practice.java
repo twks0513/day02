7e7c104721f71db78f1b5d39714da3279e82b02f
Eunhyurck <Ddoo.b.b>
2022-05-16 21:10:37 +0900

    day02

--- src/day02/Practice.java
+++ src/day02/Practice.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Practice {
 
 	public static void main(String[] args) {
-		// 1. 수를 입력하고 그 수가 몇의 배수인지 출력해주는 프로그램을 만드시오  
+		// 1. 첫번째 수를 입력하고 두번째 수를 입력한뒤 첫번째수가 두번째수의 배수인지 출력해주는 프로그램을 만드시오  
 
-		
+		// 2. 수를 입력하고 그 수가 3과 5의 공배수인지 출력해주는 프로그램을 만드시오 
 		
 		
 		//1.
@@ -22,6 +22,25 @@ public class Practice {
 		}
 		if(num1 % num2 != 0) {
 			System.out.println(num1+"은"+num2+"의 배수가 아닙니다");
+		}
+		
+		//2.
+		
+		System.out.print("수 입력 : ");
+		int su1 = scan.nextInt();
+		
+		
+		if(su1 % 3 == 0 && su1 % 5 == 0) {
+			System.out.println(su1+"은 3과 5의 공배수가 맞습니다");
+		}
+		if(su1 % 3 != 0 && su1 % 5 == 0) {
+			System.out.println(su1+"은 3과 5의 공배수가 아닙니다");
+		}
+		if(su1 % 3 == 0 && su1 % 5 != 0) {
+			System.out.println(su1+"은 3과 5의 공배수가 아닙니다");
+		}
+		if(su1 % 3 != 0 && su1 % 5 != 0) {
+			System.out.println(su1+"은 3과 5의 공배수가 아닙니다");
 		}
 		
 
